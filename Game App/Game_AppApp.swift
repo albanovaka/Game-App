11dbea9fae16d1e2385a3fd2825e71cfc8839458
albanovaka <albanova.kanykey@courrier.uqam.ca>
2024-03-15 16:01:49 -0400

    Initial Commit

---
+++ Game App/Game_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_AppApp.swift
+//  Game App
+//
+//  Created by Kanykey Albanova on 2024-03-15.
+//
+
+import SwiftUI
+
+@main
+struct Game_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
